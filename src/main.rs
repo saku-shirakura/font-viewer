ef11acdf402d5e2473063702e82ec6e389d8caf0
saku shirakura <saku@sakushira.com>
2026-01-25 16:27:38 +0900

    ピンを設定する機能の追加。及び、フィルタ機能の強化。

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use crate::font_manager::get_global_font_list;
 use glob::glob;
 use iced::font::Weight;
 use iced::widget::scrollable;
 use iced::{widget, Element, Font, Length, Task};
+use std::collections::HashSet;
 use std::fmt::{Display, Formatter};
 use std::fs::read;
 
@@ -31,6 +33,9 @@ enum Message {
     WeightChanged(FVWeight),
     TextSizeChanged(f32),
     FontFamilyFilterChanged(String),
+    Pin(usize),
+    HideUnpin(bool),
+    UpdateShownFontFamily,
 }
 
 #[derive(Clone)]
@@ -110,27 +115,60 @@ struct FontViewer {
     cb_weight: widget::combo_box::State<FVWeight>,
     text_size: f32,
     font_family_filter: String,
+    font_families: &'static Vec<String>,
+    pinned: Vec<usize>,
+    font_family_filter_pattern: Vec<String>,
+    hide_unpin: bool,
+    shown_family: Vec<usize>,
 }
 
 impl FontViewer {
-    pub fn new() -> Self {
-        Self {
-            text: "ABC123".into(),
-            weight: Default::default(),
-            cb_weight: widget::combo_box::State::new(vec![
-                FVWeight::Thin,
-                FVWeight::ExtraLight,
-                FVWeight::Light,
-                FVWeight::Normal,
-                FVWeight::Medium,
-                FVWeight::Semibold,
-                FVWeight::Bold,
-                FVWeight::ExtraBold,
-                FVWeight::Black,
-            ]),
-            text_size: 12f32,
-            font_family_filter: Default::default(),
+    pub fn new() -> (Self, Task<Message>) {
+        (
+            Self {
+                text: "ABC123".into(),
+                weight: Default::default(),
+                cb_weight: widget::combo_box::State::new(vec![
+                    FVWeight::Thin,
+                    FVWeight::ExtraLight,
+                    FVWeight::Light,
+                    FVWeight::Normal,
+                    FVWeight::Medium,
+                    FVWeight::Semibold,
+                    FVWeight::Bold,
+                    FVWeight::ExtraBold,
+                    FVWeight::Black,
+                ]),
+                text_size: 12f32,
+                font_family_filter: Default::default(),
+                font_families: get_global_font_list(),
+                pinned: vec![],
+                font_family_filter_pattern: vec![],
+                hide_unpin: false,
+                shown_family: vec![],
+            },
+            Task::done(Message::UpdateShownFontFamily),
+        )
+    }
+
+    pub fn compile_font_family_filter(&mut self) {
+        self.font_family_filter_pattern = self
+            .font_family_filter
+            .split("|")
+            .map(|v| v.to_string())
+            .collect();
+    }
+
+    pub fn is_target_font_family(&self, family: &String) -> bool {
+        if self.font_family_filter_pattern.is_empty() {
+            return true;
         }
+        for p in &self.font_family_filter_pattern {
+            if family.contains(p) {
+                return true;
+            }
+        }
+        false
     }
 
     pub fn update(&mut self, message: Message) -> Task<Message> {
@@ -146,6 +184,40 @@ impl FontViewer {
             }
             Message::FontFamilyFilterChanged(f) => {
                 self.font_family_filter = f;
+                self.compile_font_family_filter();
+                return Task::done(Message::UpdateShownFontFamily);
+            }
+            Message::Pin(id) => {
+                if self.pinned.contains(&id) {
+                    let _: Vec<usize> = self.pinned.extract_if(.., |i| *i == id).collect();
+                } else {
+                    self.pinned.push(id);
+                    self.pinned.sort();
+                    self.pinned.dedup();
+                }
+                return Task::done(Message::UpdateShownFontFamily);
+            }
+            Message::HideUnpin(v) => {
+                self.hide_unpin = v;
+                return Task::done(Message::UpdateShownFontFamily);
+            }
+            Message::UpdateShownFontFamily => {
+                self.shown_family = self
+                    .font_families
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, v)| self.is_target_font_family(v))
+                    .map(|(i, _)| i)
+                    .collect();
+                if self.hide_unpin {
+                    let filtered_families =
+                        HashSet::<_>::from_iter(self.shown_family.iter().map(|v| *v as u64));
+                    let pinned = HashSet::from_iter(self.pinned.iter().map(|v| *v as u64));
+                    self.shown_family = filtered_families
+                        .intersection(&pinned)
+                        .map(|v| usize::try_from(*v).unwrap())
+                        .collect();
+                }
             }
         }
         Task::none()
@@ -174,9 +246,13 @@ impl FontViewer {
                     Some(&self.weight),
                     Message::WeightChanged
                 )
+                .width(Length::Fixed(120f32))
             ],
             widget::row![
                 "Size:",
+                widget::text_input("text size", self.text_size.to_string().as_str())
+                    .on_input(|v| Message::TextSizeChanged(v.parse().unwrap_or(self.text_size)))
+                    .width(Length::Fixed(60f32)),
                 widget::slider(10f32..=200f32, self.text_size, Message::TextSizeChanged)
             ],
             widget::row![
@@ -184,26 +260,31 @@ impl FontViewer {
                 widget::text_input("font family", &self.font_family_filter)
                     .on_input(Message::FontFamilyFilterChanged)
             ],
+            widget::row![
+                "pinned",
+                widget::toggler(self.hide_unpin).on_toggle(Message::HideUnpin)
+            ],
             scrollable(iced::widget::table(
                 vec![
-                    widget::table::column(th("Font Family"), |font_family: &String| {
-                        widget::text(font_family)
+                    widget::table::column(th("Font Family"), |i: &usize| {
+                        widget::text(self.font_families.get(*i).unwrap())
                     }),
-                    widget::table::column(th("Text"), |font_family: &String| {
+                    widget::table::column(th("Text"), |i: &usize| {
                         widget::text(self.text.as_str())
                             .size(self.text_size)
                             .font(Font {
                                 weight: self.weight.clone().into(),
-                                ..Font::with_name(font_family)
+                                ..Font::with_name(self.font_families.get(*i).unwrap())
                             })
                     })
                     .width(Length::Fill),
+                    widget::table::column(th("Pin"), |i: &usize| {
+                        widget::toggler(self.pinned.contains(i)).on_toggle(|_| Message::Pin(*i))
+                    })
                 ],
-                font_manager::get_global_font_list()
-                    .iter()
-                    .filter(|v| self.font_family_filter.is_empty()
-                        || v.find(self.font_family_filter.as_str()).is_some()),
+                self.shown_family.iter(),
             ))
+            .spacing(3)
             .width(Length::Fill)
             .height(Length::Fill)
         ]
